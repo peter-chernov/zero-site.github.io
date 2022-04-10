dadd8ccd9afbe05b282952e9c3fb090260c88935
zero-site <petr.s.chernov@gmail.com>
2022-04-10 17:01:42 +0300

    commit

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 import {Gpio} from 'onoff-debug';
-const pin = new Gpio(2, 'out', 'both', {reconfigureDirection: false});
-if (pin.readSync() === 1) {
-    pin.writeSync(0);
-} else {
-    pin.writeSync(1);
-}
+const pin2 = new Gpio(2, 'out', 'both', {reconfigureDirection: false});
+const pin3 = new Gpio(3, 'out', 'both', {reconfigureDirection: false});
+const pin4 = new Gpio(4, 'out', 'both', {reconfigureDirection: false});
+pin2.writeSync(0);
+pin3.writeSync(0);
+pin4.writeSync(0);
+let numb = 1;
+setInterval(function() {
+    if (numb === 1) {
+        pin4.writeSync(0);
+        pin2.writeSync(1);
+    } else if(numb === 2) {
+        pin2.writeSync(0);
+        pin3.writeSync(1);
+    } else if(numb === 3) {
+        pin2.writeSync(0);
+        pin3.writeSync(1);
+        numb = 0;
+    }
+    numb = numb++;
+}, 1000);
 console.log(pin.readSync());
