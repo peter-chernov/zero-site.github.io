a0f72b50a130131deab30498f157d6fd8d5a6e5c
zero-site <petr.s.chernov@gmail.com>
2022-04-10 16:09:57 +0300

    123

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-#!/usr/bin/env node
-import process from 'process';
-console.log(process.argv[2]);
+import {Gpio} from 'onoff-debug';
+const pin = new Gpio(parseInt(process.argv[2]), 'out', 'both', {reconfigureDirection: false});
+if (pin.readSync() === 1) {
+    pin.writeSync(0);
+} else {
+    pin.writeSync(1);
+}
+console.log(pin.readSync());
