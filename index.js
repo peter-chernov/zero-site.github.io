b7ae9933def435f0b57d8cf86fb63b21f800e488
zero-site <petr.s.chernov@gmail.com>
2022-04-16 15:47:00 +0300

    com

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
-import {Gpio} from 'onoff-debug';
-const pin2 = new Gpio(2, 'out', 'both', {reconfigureDirection: false});
-const pin3 = new Gpio(3, 'out', 'both', {reconfigureDirection: false});
-const pin4 = new Gpio(4, 'out', 'both', {reconfigureDirection: false});
-pin2.writeSync(0);
-pin3.writeSync(0);
-pin4.writeSync(0);
-let numb = 1;
-setInterval(function() {
-    if (numb === 1) {
-        pin4.writeSync(0);
-        pin2.writeSync(1);
-    } else if(numb === 2) {
-        pin2.writeSync(0);
-        pin3.writeSync(1);
-    } else if(numb === 3) {
-        pin2.writeSync(0);
-        pin3.writeSync(1);
-        numb = 0;
-    }
-    numb = numb++;
-}, 1000);
+const Gpio = require('onoff').Gpio;
+const led = new Gpio(2, 'out');
+const button = new Gpio(3, 'in', 'both');
+
+button.watch((err, value) => led.writeSync(value));
